fd3d871e1ee9a24889bcff035e21820cbed6c169
root <gmmaduchukwu@gmail.com>
2025-09-29 08:55:21 -0700

    Update for more precision

--- buf_string.c
+++ buf_string.c
@@ -8,7 +8,6 @@
  *
  * Return: number of characters written
  */
-
 int buf_string(char *str, char *buffer, int *buf_index, format_flags flags)
 {
 	int len, count = 0, pad, i;
@@ -18,19 +17,22 @@ int buf_string(char *str, char *buffer, int *buf_index, format_flags flags)
 
 	len = strlen(str);
 
-	/* Apply precision */
 	if (flags.precision >= 0 && flags.precision < len)
 		len = flags.precision;
 
+	/* Calculate padding */
 	pad = (flags.width > len) ? flags.width - len : 0;
 
+	/* If not left-justified, pad first */
 	if (!flags.minus)
 		for (i = 0; i < pad; i++)
 			count += buf_add(buffer, buf_index, ' ');
 
+	/* Add the string (respecting precision) */
 	for (i = 0; i < len; i++)
 		count += buf_add(buffer, buf_index, str[i]);
 
+	/* If left-justified, pad after */
 	if (flags.minus)
 		for (i = 0; i < pad; i++)
 			count += buf_add(buffer, buf_index, ' ');
